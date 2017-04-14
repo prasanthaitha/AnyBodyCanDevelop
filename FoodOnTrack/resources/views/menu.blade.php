2aaaeb18693930a4646d2baf146525a4e9511171
ankur-123 <ankuragrawal72@gmail.com>
2017-04-14 10:21:47 +0530

    check box

--- FoodOnTrack/resources/views/menu.blade.php
+++ FoodOnTrack/resources/views/menu.blade.php
@@ -5,14 +5,18 @@
 </head>
 <body>
 
+
+<form action="/order" method="post">
   <ul>
 @foreach($foo as $foo1)
 <li>
-{{$foo1->dish_name}}
+<input type="checkbox" name="dish" value={{$foo1->dish_name}}>{{$foo1->dish_name}}</input>
 {{$foo1->price}}</li>
+<input type="integer"  name="q{{$foo1->dish_name}}">
 @endforeach
+<input type="submit" name="submit" value="Submit"/>
 </ul>
-
+</form>
 
 
 </body>
